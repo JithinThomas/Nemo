cefa6b494bac317233aa0fbb36d54c216893cb35
Jithin Thomas <jthomas@Jithins-MacBook-Pro.local>
2015-10-11 19:18:22 -0700

    Fixing bugs in watching the shapes collection

--- directives/workspace.js
+++ directives/workspace.js
@@ -1,14 +1,4 @@
 
-function removeShapes(canvas) {
-  canvas.clear();
-}
-
-function drawShapes(canvas, shapes) {
-  shapes.forEach(function(s) {
-    canvas.ellipse( s.cx, s.cy, s.rx, s.ry ).attr({ fill: "darkgreen" });;
-  });
-}
-
 directivesModule.directive('workspace', [function() {
   return {
     restrict: 'EA',
@@ -18,12 +8,45 @@ directivesModule.directive('workspace', [function() {
     },
 
     link: function(scope, element, attrs) {
-      scope.canvas = Raphael(100, 10, 750, 300);
+      scope.canvas = Raphael(100, 10, 750, 700);
+
+      scope.shapes.forEach(function(s, i) {
+        scope.$watchCollection('shapes[' + i + ']', function(n) {
+          redrawShape(scope.canvas, n)
+        })
+      });
 
       scope.$watchCollection('shapes', function(newVal, oldVal) {
         removeShapes(scope.canvas);
         drawShapes(scope.canvas, newVal);
+
+        for (var i = oldVal.length; i < newVal.length; i++) {
+          scope.$watchCollection('shapes[' + i + ']', function(n) {
+            redrawShape(scope.canvas, n)
+          }, true);
+        }
       }, true);
     }
   };
 }]);
+
+//===========================================
+// Helper functions
+//===========================================
+
+function removeShapes(canvas) {
+  canvas.clear();
+}
+
+function drawShapes(canvas, shapes) {
+  shapes.forEach(function(s) {
+    redrawShape(canvas, s);
+  });
+}
+
+function redrawShape(canvas, s) {
+  var id = "s-" + s.i;
+  $("#" + id).remove();
+  var e = canvas.ellipse( s.cx, s.cy, s.rx, s.ry ).attr({ fill: "darkgreen" });
+  $(e.node).attr('id', id);
+}
--- workspace/workspace_controller.js
+++ workspace/workspace_controller.js
@@ -3,12 +3,21 @@ myApp.controller('WorkSpaceCtrl', function($scope) {
   $scope.c = 10;
   $scope.r = 5;
 
-  $scope.shapesList = [{ "cx" : $scope.c, "cy" : $scope.c, "rx" : $scope.r, "ry": $scope.r }];
+  $scope.shapesList = [
+    { "cx" : $scope.c, "cy" : $scope.c, "rx" : $scope.r, "ry": $scope.r, "fill": "red", "i": 0 },
+    { "cx" : $scope.c, "cy" : $scope.c + 10, "rx" : $scope.r, "ry": $scope.r, "fill": "red", "i": 1 },
+    { "cx" : $scope.c, "cy" : $scope.c + 20, "rx" : $scope.r, "ry": $scope.r, "fill": "red", "i": 2 },
+    { "cx" : $scope.c, "cy" : $scope.c + 30, "rx" : $scope.r, "ry": $scope.r, "fill": "red", "i": 3 }
+  ];
 
   $scope.foo = function() {
     $scope.c += 20;
+
     $scope.shapesList.push({
-      "cx" : $scope.c, "cy" : $scope.c, "rx" : $scope.r, "ry": $scope.r
-    })
+      "cx" : $scope.c, "cy" : $scope.c, "rx" : $scope.r, "ry": $scope.r, "fill": "red", "i": $scope.shapesList.length
+    });
+    
+    var i = Math.round( Math.random() * ($scope.shapesList.length - 1));
+    $scope.shapesList[i].cx = $scope.c;
   };
 });
