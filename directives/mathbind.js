014e89fac04e54c230d6ed8c7af2573600a161af
Robin Paul <robinpaulp@gmail.com>
2015-10-27 01:31:38 -0700

    Fix for flicker issue

--- directives/mathbind.js
+++ directives/mathbind.js
@@ -4,12 +4,24 @@ directivesModule.directive("mathBind", function() {
     return {
         restrict: "A",
         controller: ["$scope", "$element", "$attrs", function($scope, $element, $attrs) {
+            var math = null;
             $scope.$watch($attrs.mathBind, function(value) {
-                var $script = angular.element("<script type='math/tex'>")
-                    .html(value == undefined ? "" : value);
-                $element.html("");
-                $element.append($script);
-                MathJax.Hub.Queue(["Reprocess", MathJax.Hub, $element[0]]);
+                // var $script = angular.element("<script type='math/tex'>")
+                    // .html(value == undefined ? "" : value);
+                // $element.html("");
+                // $element.append($script);
+                // var el = $element[0];
+                // var math = MathJax.Hub.getAllJax($element[0])[0];
+                // MathJax.Hub.Queue(["Typeset", MathJax.Hub, $element[0]]);
+                // MathJax.Hub.Queue(["Text", math, value]);
+                if (math === null) {
+                    MathJax.Hub.queue.Push(function () {
+                          math = MathJax.Hub.getAllJax($element[0])[0];
+                          math.Text(value);
+                        });
+                } else {
+                    math.Text(value);
+                }
             });
         }]
     };
